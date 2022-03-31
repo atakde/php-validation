f615c9a4e9ffb18349ca080220d189a4323eb91b
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2022-03-31 20:52:11 +0300

    feat(php-validation): ParameterizedTest added

---
+++ tests/ParameterizedTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Atakde\PhpValidation\Validator;
+use PHPUnit\Framework\TestCase;
+
+final class ParameterizedTest extends TestCase
+{
+    protected $validator;
+
+    protected function setUp()
+    {
+        $this->validator = new Validator;
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testAdd($params, $rules)
+    {
+        $this->validator->validate(['myInput' => 'string'], ['myInput' => 'my string']); // giving a string value
+        $this->assertTrue($this->validator->passes());
+    }
+
+    public function provider()
+    {
+        return [
+            [['myInput' => 'required|string'], ['myInput' => 'my string']],
+            [['myInput' => 'required|numeric'], ['myInput' => '123']],
+            [['myInput' => 'required|email'], ['myInput' => 'me@atakann.com']]
+        ];
+    }
+}
